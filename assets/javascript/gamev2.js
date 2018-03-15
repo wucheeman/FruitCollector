3b7f4199704959e8cf7fac30891f756ca6baffb1
Mark Hainline <wucheeman@gmail.com>
2018-03-14 21:47:53 -0400

    restructured game that works well through first round. Currently fails to reinitialize values for fruit.

--- assets/javascript/gamev2.js
+++ assets/javascript/gamev2.js
@@ -2,7 +2,7 @@
 //=======================================================
 
 // TODO: does 'fruit' need to be global?
-var fruit;
+var fruitClasses;
 var displayFruit
 
 var targetNum;
@@ -52,21 +52,30 @@ function initializeDisplay() {
 
 function initalizeGlobals() {
   // initializes values for newly started game
+  console.log("in initializeGlobals")
   targetNum = setTargetNum();
-  fruit = setUpFruit();
   displayFruit = $(".fruit");
   currentScore = 0;
   playAnother = false;
-  initializeWinsAndLosses();
-}
-
-function initializeWinsAndLosses() {
-  // sets wins and losses to 0 when game starts
   if (!continuing) {
+    fruitClasses = setUpFruit();
+    console.log("with continuing = false, FruitClasses is " + fruitClasses);
     wins = 0;
     losses = 0;
   }
+  else {
+    console.log("with continuing = true, FruitClasses is " + fruitClasses);
+    reInitializeFruit();
+  }
 }
+
+// function initializeWinsAndLosses() {
+//   // sets wins and losses to 0 when game starts
+//   if (!continuing) {
+//     wins = 0;
+//     losses = 0;
+//   }
+// }
 
 function logVals() {
   // TODO comment out when development is done
@@ -79,21 +88,22 @@ function logVals() {
 }
 
 function main() {
+  console.log("in main")
   setUpGame();
-  playRound();
-  continuing = true;
-  if (playAnother) {
-    nextRound();
-  }
+  play();
+  console.log('finished play and still running');
 }
 
-function nextRound(){
-  main();
-}
+// TODO: delete
+// function nextRound(){
+  // console.log('in nextRound');
+  // main();
+// }
 
-function playRound() {
-  // functionality for a single round of the game
+function play() {
+  // play the game until the window is closed
   displayFruit.on("click", ".fruitImage", function() {
+    console.log('starting playRound');
     var fruitValue = ($(this).attr("data-fruitvalue"));
     fruitValue = parseInt(fruitValue);
     console.log("This fruit's value is: " + fruitValue);
@@ -101,7 +111,30 @@ function playRound() {
     // updateCurrentScoreDisplay();
     displayCurrentScore();
     winLoseOrGoOn();
+    continuing = true;
+    if (playAnother) {
+      console.log("starting a new game");
+      setUpGame();
+    }
+    console.log('at end of play\'s loop');
   }); // close of on-click function
+}
+
+function reInitializeFruit() {
+  // var fruitClasses = [];
+  // for (var j = 0; j < fruitClasses.length; j++) {
+  //   fruitClasses.push("." + fruitClasses[j]);
+  //   console.log("fruitClasses[" + j + "] is " + fruitClasses[j]);
+  // }
+  console.log('reinitiazling fruit nums');
+  var fruitNums = setFruitNums();
+  console.log('fruitNums is ' + fruitNums);
+  for (var i = 0; i < fruitClasses.length; i++) {
+    console.log($(fruitClasses[i]).attr("data-fruitvalue"));
+    $(fruitClasses[i]).attr("data-fruitvalue", fruitNums[i]);
+    console.log($(fruitClasses[i]).attr("data-fruitvalue"));
+  }
+  console.log("exiting reInitializeFruit")
 }
 
 function setFruitNums() {
@@ -120,12 +153,6 @@ function setTargetNum() {
 }
 
 function setUpFruit() {
-  var apple;
-  var lemon;
-  var pear;
-  var orange;
-  fruit = [apple, lemon, pear, orange];
-
   // values for HTML src attribute
   var fruitSrcs = ["assets/images/apple.png",
                    "assets/images/lemon.png",
@@ -137,7 +164,7 @@ function setUpFruit() {
   // HTML class attributes
   var fruitClasses = [".apple", ".lemon", ".pear", ".orange"];
 
-  for (var i = 0; i < fruit.length; i++) {
+  for (var i = 0; i < fruitClasses.length; i++) {
     var fruitForGame = $("<img>");
     fruitForGame.addClass("fruitImage");
     fruitForGame.attr("src", fruitSrcs[i]);
@@ -145,11 +172,13 @@ function setUpFruit() {
     var fruitClass = fruitClasses[i];
     $(fruitClass).append(fruitForGame);
   }
+  return fruitClasses;
 }
 
 function setUpGame() {
   // Sets up play of game
   initalizeGlobals();
+  // TODO - comment out
   logVals();
   initializeDisplay();
 }
@@ -166,12 +195,14 @@ function winLoseOrGoOn(){
   // updates browser window when player wins or loses
   // TODO: replace alerts with real functionality
   // TODO: add logic to restart game after win or loss?
+  // TODO: fix bug which outputs 'you win' before updating the display with the final value
   if (currentScore === targetNum) {
     // TODO: better way to do this? Make it DRY?
     alert("You win!");
     wins++;
     displayWins();
     playAnother = true;
+    return;
   }
   else if (currentScore > targetNum) {
     // TODO: better way to do this?
@@ -179,6 +210,7 @@ function winLoseOrGoOn(){
     losses++;
     displayLosses();
     playAnother = true;
+    return;
   }
 }
 
@@ -191,4 +223,11 @@ $(document).ready(function() {
 
 
 // RESUME: 
-// defer giveing targetNum a random value until game is ready to complete
+/*
+Restructured game basically works, BUT although I generate new values for each fruit
+I am not assigning those values correctly. When the game restarts, each fruit has the
+value assigned in the first game. 
+The problem is in reInitializeFruit; I don't know how to access dynamically generated elements and change the value of an attribute. Trying v3 to see if I can use static elements.
+Once this works, review TODOs. At a minimum, fix the bug noted in winLoseOrGoOn
+*/
+// defer giving targetNum a random value until game is ready to complete
